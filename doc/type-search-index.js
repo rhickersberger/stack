4d90d12c7441b6bb5cbf37f362fb3c90d11e2fe6
René Hickersberger <rhickersberger@student.tgm.ac.at>
2021-01-04 20:02:22 +0100

    Dokumentation hinzufügen

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"rhickersberger","l":"Stack"},{"p":"rhickersberger","l":"StackEmptyException"},{"p":"rhickersberger","l":"StackFullException"},{"p":"rhickersberger","l":"StackTest"}]
